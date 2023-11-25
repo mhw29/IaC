66aea5152f9f09314ab7ee5cd24ff5e4cb2f4a55
Matt Williamson <mahwill29@gmail.com>
2023-11-25 07:51:31 -0600

    Fixing up logging.

--- sample_resources/main.tf
+++ sample_resources/main.tf
@@ -103,15 +103,16 @@ resource "aws_security_group" "security_group" {
 }
 
 ########################EC2 Instance############################################
+
 resource "aws_launch_template" "ecs_lt" {
  name_prefix   = "ecs-template"
- image_id      = "ami-0230bd60aa48260c6"
+ image_id      = "ami-0b74aeb97fba885ea"
  instance_type = "t3.micro"
 
- key_name               = "ec2ecsglog"
+ key_name               = "dell_machine"
  vpc_security_group_ids = [aws_security_group.security_group.id]
  iam_instance_profile {
-   name = "ecsInstanceRole"
+   name ="ecsInstanceRole"
  }
 
  block_device_mappings {
@@ -134,8 +135,8 @@ resource "aws_launch_template" "ecs_lt" {
 
 resource "aws_autoscaling_group" "ecs_asg" {
  vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet-2.id]
- desired_capacity    = 2
- max_size            = 3
+ desired_capacity    = 1
+ max_size            = 1
  min_size            = 1
 
  launch_template {
@@ -183,4 +184,133 @@ resource "aws_lb_target_group" "ecs_tg" {
  health_check {
    path = "/"
  }
+}
+
+########################ECS Cluster############################################
+resource "aws_ecs_cluster" "ecs_cluster" {
+ name = "my-ecs-cluster"
+}
+
+resource "aws_ecs_capacity_provider" "ecs_capacity_provider" {
+ name = "test1"
+
+ auto_scaling_group_provider {
+   auto_scaling_group_arn = aws_autoscaling_group.ecs_asg.arn
+
+   managed_scaling {
+     maximum_scaling_step_size = 1000
+     minimum_scaling_step_size = 1
+     status                    = "ENABLED"
+     target_capacity           = 2
+   }
+ }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "example" {
+ cluster_name = aws_ecs_cluster.ecs_cluster.name
+
+ capacity_providers = [aws_ecs_capacity_provider.ecs_capacity_provider.name]
+
+ default_capacity_provider_strategy {
+   base              = 1
+   weight            = 100
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+ }
+}
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name = "ecs_task_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ecs-tasks.amazonaws.com"
+        },
+      },
+    ],
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
+  role       = aws_iam_role.ecs_task_execution_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+resource "aws_cloudwatch_log_group" "ecs_log_group" {
+  name = "/ecs/my-ecs-log-group" // Replace with your desired log group name
+
+  retention_in_days = 30 // Optional: Configure log retention period (in days)
+
+  tags = {
+    Name = "ECS Log Group"
+  }
+}
+
+
+resource "aws_ecs_task_definition" "ecs_task_definition" {
+ family             = "my-ecs-task"
+ network_mode       = "awsvpc"
+ execution_role_arn = aws_iam_role.ecs_task_execution_role.arn
+ cpu                = 256
+ runtime_platform {
+   operating_system_family = "LINUX"
+   cpu_architecture        = "X86_64"
+ }
+ container_definitions = jsonencode([
+   {
+     name      = "dockergs"
+     image     = "public.ecr.aws/f9n5f1l7/dgs:latest"
+     cpu       = 256
+     memory    = 512
+     essential = true
+     portMappings = [
+       {
+         containerPort = 80
+         hostPort      = 80
+         protocol      = "tcp"
+       }
+     ]
+    logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+            awslogs-group         = aws_cloudwatch_log_group.ecs_log_group.name
+            awslogs-region        = "us-east-1"
+            awslogs-stream-prefix = "ecs"
+        }
+    }
+   }
+ ])
+}
+
+resource "aws_ecs_service" "ecs_service" {
+ name            = "my-ecs-service"
+ cluster         = aws_ecs_cluster.ecs_cluster.id
+ task_definition = aws_ecs_task_definition.ecs_task_definition.arn
+ desired_count   = 2
+
+ network_configuration {
+   subnets         = [aws_subnet.subnet.id, aws_subnet.subnet-2.id]
+   security_groups = [aws_security_group.security_group.id]
+ }
+
+ force_new_deployment = true
+ placement_constraints {
+   type = "distinctInstance"
+ }
+
+ capacity_provider_strategy {
+   capacity_provider = aws_ecs_capacity_provider.ecs_capacity_provider.name
+   weight            = 100
+ }
+
+ load_balancer {
+   target_group_arn = aws_lb_target_group.ecs_tg.arn
+   container_name   = "dockergs"
+   container_port   = 80
+ }
+
+ depends_on = [aws_autoscaling_group.ecs_asg]
 }
