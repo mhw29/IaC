10fe0458777b38d50cdf3919dc752237b8cf578c
Matt Williamson <mahwill29@gmail.com>
2023-12-10 08:54:54 -0600

    Adding variables.

--- sample_resources/eks/variables.tf
+++ sample_resources/eks/variables.tf
@@ -2,5 +2,5 @@
 variable "subnet_ids" {
   description = "List of subnet IDs"
   type        = list(string)
-  default     = ["subnet-0d0b5b581c5afaca5", "subnet-05b09161909c9336e"]
+  default     = [""]
 }
